fcd8274625cede9bbb941e99720ef148e9690b3d
RikuKH3 <riku.kh3@gmail.com>
2017-10-05 23:29:38 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+Notabenoid Downloader
+=====================
+Usage: notadl.exe &lt;login&gt; &lt;password&gt; &lt;book&gt;
---
+++ notadl.dpr
@@ -0,0 +1,73 @@
+﻿program notadl;
+
+{$WEAKLINKRTTI ON}
+{$RTTI EXPLICIT METHODS([]) PROPERTIES([]) FIELDS([])}
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  Windows, System.SysUtils, System.Classes, IdHTTP;
+
+{$SETPEFLAGS IMAGE_FILE_RELOCS_STRIPPED}
+
+var
+  MemoryStream1: TMemoryStream;
+  IdHTTP1: TIdHTTP;
+  StringList1: TStringList;
+  i, x, y: Integer;
+  s, ChapterId, ChapterName, OutDir: String;
+begin
+  try
+    Writeln('Notabenoid Downloader v1.0 by RikuKH3');
+    Writeln('-------------------------------------');
+    if ParamCount<3 then begin Writeln('Usage: '+ExtractFileName(ParamStr(0))+' <login> <password> <book>'); Readln; exit end;
+
+    StringList1:=TStringList.Create; MemoryStream1:=TMemoryStream.Create;
+    try
+      StringList1.Append('login[login]='+ParamStr(1));
+      StringList1.Append('login[pass]='+ParamStr(2));
+      IdHTTP1:=TIdHTTP.Create(nil);
+      try
+        IdHTTP1.AllowCookies := True;
+        IdHTTP1.HandleRedirects := True;
+        IdHTTP1.Post('http://notabenoid.org', StringList1);
+        IdHTTP1.Get('http://notabenoid.org/book/'+ParamStr(3), MemoryStream1);
+        MemoryStream1.Position := 0;
+        StringList1.LoadFromStream(MemoryStream1, TEncoding.UTF8);
+        MemoryStream1.Clear;
+
+        x := 0;
+        for i:=0 to StringList1.Count-1 do begin
+          x := Pos('<tr id='#39'c_', StringList1[i]);
+          if x>0 then begin
+            s:=StringList1[i];
+            OutDir := ExtractFilePath(ParamStr(0))+'book_'+ParamStr(3);
+            if not (DirectoryExists(OutDir)) then CreateDir(OutDir);
+            break
+          end
+        end;
+
+        while x>0 do begin
+          x := Pos('<a href=', s);
+          s := Copy(s, x+9);
+          i := Pos(#39, s);
+          ChapterId := Copy(s, 1, i-1);
+          s := Copy(s, i+2);
+          ChapterName := Copy(s, 1, Pos('</a>',s)-1);
+          x := Pos('<tr id='#39'c_', s);
+          s := Copy(s, x);
+          IdHTTP1.Get('http://notabenoid.org'+ChapterId+'/download?format=t&enc=UTF-8', MemoryStream1);
+          MemoryStream1.Position := 0;
+          StringList1.LoadFromStream(MemoryStream1);
+          MemoryStream1.Clear;
+          for i:=StringList1.Count-1 downto 0 do if StringList1[i]='Переведено на Нотабеноиде' then break;
+          if i>0 then if i-3>=1 then if StringList1[i-3]='Внимание! Этот перевод, возможно, ещё не готов.' then for y:=0 to StringList1.Count-i+3 do StringList1.Delete(StringList1.Count-1) else else for y:=0 to StringList1.Count-i do StringList1.Delete(StringList1.Count-1);
+          Writeln(OutDir+'\'+ChapterName+'.txt');
+          StringList1.SaveToFile(OutDir+'\'+ChapterName+'.txt', TEncoding.UTF8);
+        end;
+      finally IdHTTP1.Free end;
+    finally StringList1.Free; MemoryStream1.Free end;
+  except on E: Exception do begin Writeln(E.Message); Readln end end;
+end.
